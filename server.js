de9a3909c60aa583c3c40ccb0af883f5cbf94391
Rajdeep Basak <90843663+rrbasak@users.noreply.github.com>
2022-03-05 02:17:36 +0530

    change

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const port=process.env.PORT || 3000
 
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'public'));
 
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html')
+    res.sendFile(__dirname+'index.html')
 })
 
 http.listen(port,()=>{
